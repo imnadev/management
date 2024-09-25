f01d3d13069a559d650dd12cc6212a06dc3f00cc
imnadev <imnadev@gmail.com>
2024-09-25 14:26:03 +0500

    Fixed closing manager

--- lib/src/state_management/manager.dart
+++ lib/src/state_management/manager.dart
@@ -6,19 +6,24 @@ abstract class Manager<STATE, EFFECT> {
 
   STATE get state => stateSubject.value;
 
+  bool isClosed = false;
+
   Manager(STATE initialState)
       : stateSubject = BehaviorSubject<STATE>.seeded(initialState);
 
   void emit(STATE state) {
+    if (stateSubject.isClosed) return;
     stateSubject.add(state);
   }
 
   void publish(EFFECT effect) {
+    if (effectSubject.isClosed) return;
     this.effectSubject.add(effect);
   }
 
   Future<void> close() async {
     stateSubject.close();
     effectSubject.close();
+    isClosed = true;
   }
 }
